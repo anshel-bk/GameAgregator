997566f072062f0f952d9f064d6d25f218c00e40
Dmitry Shmyrin <barackakamacho@gmail.com>
2022-07-26 21:20:25 +0700

    Поменял директории медиа-папок, сделал вывод статей на главную

--- GameAgregator/settings.py
+++ GameAgregator/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+print(MEDIA_ROOT)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- game_info_part/views.py
+++ game_info_part/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Article
 from django.views.generic import TemplateView
 
 
@@ -8,5 +9,6 @@ class HomePage(TemplateView):
     template_name = "game_info_part/home.html"
 
     def get_context_data(self, **kwargs):
-        context = {"title": "Главная страница"}
+        articles = Article.objects.all()
+        context = {"title": "Главная страница","articles":articles}
         return context
