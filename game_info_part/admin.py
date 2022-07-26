72519b47bc9a8a7a868d5dd16e89cb6e1611de3e
Dmitry Shmyrin <barackakamacho@gmail.com>
2022-07-26 16:39:50 +0700

    Подключил таблицы в админку поменял тайм зону

--- game_info_part/admin.py
+++ game_info_part/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from game_info_part.models import AuthorArticle, Article
+
 
 # Register your models here.
+
+@admin.register(Article)
+class AdminArticle(admin.ModelAdmin):
+    list_display = ('author', 'name_game', 'title', 'preview_picture', 'rating',)
+    list_display_links = ('title',)
+    search_fields = ('title', 'loot')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(AuthorArticle)
+class AdminAuthorArticle(admin.ModelAdmin):
+    list_display = ('author',)
+    search_fields = ('author',)
+
